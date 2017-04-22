b396bbb0fb5cf425af0884fd5f342fb49331c0f1
Kunanon Assawarodjanamitre <kunanon.assa@bumail.net>
2017-04-22 15:39:25 +0700

    ข้อ 6

--- SpaceShooter/Assets/Script/PlayerController.cs
+++ SpaceShooter/Assets/Script/PlayerController.cs
@@ -20,13 +20,23 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	public Rigidbody rigidbody;
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
 
 	void FixedUpdate ()
 	{
