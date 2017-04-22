226a34035dc785b08ea2fdc2c7a3bc0ef316cc0a
Kunanon Assawarodjanamitre <kunanon.assa@bumail.net>
2017-04-22 15:54:41 +0700

    ข้อ 8

---
+++ SpaceShooter/Assets/Script/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public Rigidbody rigidbody;
+
+	// Use this for initialization
+	public float tumble;
+
+	void Start ()
+	{
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
